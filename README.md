9f17982d9b23a6d3fbdf200075c014ce358d6a01
abdulsamadkhan <samad19472002@gmail.com>
2023-07-26 18:18:57 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Product-Catalogue
+ Advertisement using ChatGPT
---
+++ env.js
@@ -0,0 +1,6 @@
+export const process = {
+    env: {
+        OPENAI_API_KEY: "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"
+    }
+}
+
